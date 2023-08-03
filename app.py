64873ccdb0bedf88fe8a81affe97b4974e2d30b3
justinstoner2 <justinstoner2@gmail.com>
2023-08-03 06:51:21 -0700

    insert and delete for campaigns

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
 from flask_navigation import Navigation
 import database.db_connector as db
 import os
@@ -8,6 +10,7 @@ import os
 app = Flask(__name__)
 nav = Navigation(app)
 db_connection = db.connect_to_database()
+mysql = MySQL(app)
 
 #initializing Navigations
 nav.Bar('top',[
@@ -27,59 +30,118 @@ nav.Bar('top',[
 def index():
     return render_template("index.j2")
 
-@app.route('/customers')
+@app.route('/customers', methods = ["POST", "GET"])
 def customers():
-    query = "SELECT * FROM Customers;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    return render_template("customers.j2", customers=results)
+    if request.method == 'GET':
+        query = "SELECT * FROM Customers;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template("customers.j2", customers=results)
 
-@app.route('/campaigns')
+@app.route('/deleteCustomer/<int:id>')
+def deleteCustomers(id):
+    
+    query = "DELETE FROM Customers WHERE customerID = '%s';"
+    cursor = db.execute_query(db_connection = db_connection, query=query)
+    results = cursor.fet
+
+    return redirect("/customers")
+@app.route('/campaigns',methods = ["POST", "GET"])
 def campaigns():
-    query = "SELECT * FROM Campaigns;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    return render_template("campaigns.j2", campaigns = results)
+     # Create function for campaigns, relies on modal for input raw data
+    if request.method == "POST":
+        # used when the user presses the add campaign button
+        print("post is working")
+        
+        print("insert is working")
+        channelID = request.form["chidinput_dd"]
+            
+        startDate = request.form["chstartinput"]
+        endDate = request.form["chendinput"]
+        productID = request.form["pidinput_dd"]
 
-@app.route('/channels')
+        # this mess below accounts for several possible null variataions, for sanity lets default to all fields to blank (vs 0)
+        if channelID == "" and productID == "" and endDate == "":
+            query = "INSERT INTO Campaigns (startDate) VALUES (%s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+    
+        elif channelID == "" and productID == "":
+            query = "INSERT INTO Campaigns (startDate, endDate) VALUES (%s, %s);"
+            cursor = db.execute_query(db_connection=db_connection, query=query) 
+
+        elif channelID == "":
+            query = "INSERT INTO Campaigns (startDate, endDate, productID) VALUES (%s, %s, %s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+
+        elif productID == "":
+            query = "INSERT INTO Campaigns (channelID, startDate, endDate) VALUES (%s, %s, %s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+    
+        else: 
+            query = "INSERT INTO Campaigns (channelID, startDate, endDate, productID) VALUES (%s, %s, %s, %s);" 
+            print("productID"+productID)
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(channelID,startDate,endDate,productID))
+            
+            
+
+            
+        
+        # return to product page
+        print("about to redirect")
+        return redirect("/campaigns")
+    
+    if request.method == "GET":
+        query = "SELECT campaignID,  channelID, startDate, endDate, productID, (datediff(endDate, startDate) * (SELECT rate FROM Channels  WHERE Campaigns.channelID = Channels.channelID)) as cost FROM Campaigns;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template("campaigns.j2", campaigns = results)
+
+
+@app.route('/channels', methods = ["POST", "GET"])
 def channels():
     query = "SELECT * FROM Channels;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("channels.j2",channels = results)
 
-@app.route('/inventory')
+@app.route('/inventory', methods = ["POST", "GET"])
 def inventory():
     query = "SELECT * FROM Inventory;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("inventory.j2", inventory=results)
 
-@app.route('/products')
+@app.route('/products', methods = ["POST", "GET"])
 def products():
     query = "SELECT * FROM Products;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("products.j2", products=results)
 
-@app.route('/saleItems')
+@app.route('/saleItems', methods = ["POST", "GET"])
 def saleItems():
     query = "SELECT * FROM SaleItems;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("saleItems.j2", saleItems= results)
 
-@app.route('/sales')
+@app.route('/sales', methods = ["POST", "GET"])
 def sales():
     query = "SELECT * FROM Sales;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("sales.j2", sales = results)
+
+@app.route("/delete_campaign/<int:campaignID>")
+def delete_campaign(campaignID):
+    query = "DELETE FROM Campaigns WHERE campaignID = %s;"
+    db.execute_query(db_connection=db_connection, query=query, query_params=(campaignID,))
+    return redirect("/campaigns")
 # Listener
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9853)) 
+    port = int(os.environ.get('PORT', 9854)) 
     #                                 ^^^^
     #              You can replace this number with any valid port
     
